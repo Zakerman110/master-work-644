aa5660ddf617154d6c478ac9493340f7a1761a8a
Artur Hostiuk <artur1100011@gmail.com>
2024-11-05 20:59:19 +0200

    Add multithreading

--- backend/api/scrapers/scraper_manager.py
+++ backend/api/scrapers/scraper_manager.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from api.models import Product
 from api.scrapers.comfy import scrape_comfy_product
 from api.scrapers.rozetka import scrape_rozetka_product
@@ -5,47 +6,53 @@ from api.scrapers.allo import scrape_allo_product
 from api.scrapers.foxtrot import scrape_foxtrot_product
 from api.scrapers.citrus import scrape_citrus_product
 from api.utils.db_utils import save_product_to_db
-from api.utils.web_driver import get_driver
 
-
-def scrape_and_save_product(product_name, source='rozetka'):
+def scrape_and_save_product(product_name):
     """
-    First check if the product already exists in the database by name.
+    Check if the product already exists in the database by name.
     If found, return the product.
-    If not, scrape the product and save it to the database.
+    If not, scrape the product concurrently across multiple sources and save it to the database.
     """
-    # Check if the product exists in the database by name
+
     products = Product.objects.filter(name__icontains=product_name)
 
     if products.exists():
-        product = products.first()  # Get the first matching product
+        product = products.first()
         print(f"Product '{product_name}' found in the database.")
         return product
 
+    print(f"Product '{product_name}' not found in the database. Scraping now...")
+
+    scrape_functions = {
+        'rozetka': scrape_rozetka_product,
+        'comfy': scrape_comfy_product,
+        'allo': scrape_allo_product,
+        'foxtrot': scrape_foxtrot_product,
+        'citrus': scrape_citrus_product,
+    }
+
+    scraped_data = []
+    with ThreadPoolExecutor() as executor:
+        future_to_source = {
+            executor.submit(scrape_func, product_name): source
+            for source, scrape_func in scrape_functions.items()
+        }
+
+        for future in as_completed(future_to_source):
+            source = future_to_source[future]
+            try:
+                product_data = future.result()
+                if product_data:
+                    scraped_data.append(product_data)
+                    print(f"Data from {source} has been scraped successfully.")
+            except Exception as e:
+                print(f"Error scraping {source}: {e}")
+
+    if scraped_data:
+        for data in scraped_data:
+            save_product_to_db(data)
+        print(f"Data for '{product_name}' has been saved to the database.")
+        return Product.objects.filter(name__iexact=product_name).first()
     else:
-        print(f"Product '{product_name}' not found in the database. Scraping now...")
-
-        driver = get_driver()
-        if source == 'rozetka':
-            product_data = scrape_rozetka_product(product_name)
-        elif source == 'comfy':
-            product_data = scrape_comfy_product(product_name)
-        elif source == 'allo':
-            product_data = scrape_allo_product(product_name)
-        elif source == 'foxtrot':
-            product_data = scrape_foxtrot_product(product_name)
-        elif source == 'citrus':
-            product_data = scrape_citrus_product(product_name)
-        else:
-            raise ValueError(f"Source '{source}' is not supported.")
-        driver.quit()
-
-        if product_data:
-            save_product_to_db(product_data)
-
-            product = Product.objects.filter(name__iexact=product_name).first()
-            return product
-
-        else:
-            print(f"No product found for '{product_name}' after scraping.")
-            return None
+        print(f"No product found for '{product_name}' after scraping.")
+        return None
--- backend/api/utils/web_driver.py
+++ backend/api/utils/web_driver.py
@@ -1,22 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-
 import undetected_chromedriver as uc
 
 
+_driver = None
+
 def get_driver():
     """
-    Set up and return the Selenium WebDriver (Chrome) for headless scraping.
+    Set up and return a single instance of the Selenium WebDriver (Chrome).
     """
-    # chrome_options = Options()
-    # # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--no-sandbox")
-    #
-    # # Set the path to your ChromeDriver
-    # service = Service(executable_path='E:\\Programming\\Selenium\\chromedriver.exe')
-    # driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver = uc.Chrome(headless=False,use_subprocess=False)
+    global _driver
+    if _driver is None:
+        options = uc.ChromeOptions()
+        options.add_argument("--headless=new")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-application-cache")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-setuid-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        _driver = uc.Chrome(options=options, headless=False, use_subprocess=False, user_multi_procs=True)
 
-    return driver
+    return _driver
+
+
+def quit_driver():
+    global _driver
+    if _driver:
+        _driver.quit()
+        _driver = None
+
